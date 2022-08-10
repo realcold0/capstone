611e779818f8f24a70f6ff7a2c9b4fc4fcde6f69
Daekyue <howeorb@naver.com>
2022-08-10 22:02:34 +0900

    아이디 체크 후 로그인 로그인 후 메인화면 이동 구축

--- app/src/main/java/com/akj/sns_project/LoginActivity.java
+++ app/src/main/java/com/akj/sns_project/LoginActivity.java
@@ -15,20 +15,19 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SignUpActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "SignUpActivity";
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_login);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.loginButton).setOnClickListener(onClickListener);
-        findViewById(R.id.gotoLoginButton).setOnClickListener(onClickListener);
     }
 
     @Override
@@ -43,42 +42,35 @@ public class SignUpActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.loginButton:
-                    signUp();
+                    Login();
                     break;
 
-                case R.id.gotoLoginButton:
-                    startLoginActivity();
-                    break;
             }
         }
     };
 
-    private void signUp(){
+    private void Login(){
         String email = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
         String password = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
-        String passwordCheck = ((EditText)findViewById(R.id.passwordCheckEditText)).getText().toString();
 
-        if(email.length() > 0 && password.length() > 0 && passwordCheck.length() > 0){
-            if(password.equals(passwordCheck)){
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    startToast("회원가입에 성공하였습니다");
-                                    // 성공했을때 UI
-                                } else {
-                                    if (task.getException() != null) {
-                                        startToast(task.getException().toString());
-                                        // 실패했을때 UI
-                                    }
+        if(email.length() > 0 && password.length() > 0){
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startToast("로그인에 성공하였습니다");
+                                startMainActivity();
+                            } else {
+                                if (task.getException() != null) {
+                                    startToast(task.getException().toString());
+                                    // 실패했을때 UI
                                 }
                             }
-                        });
-            }else{
-                startToast("비밀번호와 비밀번호 확인이 일치하지 않습니다");
-            }
+                        }
+                    });
         }else{
             startToast("이메일 또는 비밀번호를 입력해주세요");
         }
@@ -88,8 +80,8 @@ public class SignUpActivity extends AppCompatActivity {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    private void startLoginActivity(){
-        Intent intent = new Intent(this,LoginActivity.class);
+    private void startMainActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
 }
