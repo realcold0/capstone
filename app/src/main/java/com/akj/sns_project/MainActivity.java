9d9273fe81ce9b55d8b2b012383d81791676c2fe
Daekyue <howeorb@naver.com>
2022-08-10 22:13:31 +0900

    로그아웃 기능 활성화

--- app/src/main/java/com/akj/sns_project/MainActivity.java
+++ app/src/main/java/com/akj/sns_project/MainActivity.java
@@ -2,7 +2,11 @@ package com.akj.sns_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {  // 로그인된 유저가 있는지 없는지 체크 후 없을 경우 회원가입 페이지부터 시작
+            startSignUpActivity();
+        }
+        findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.logoutButton:
+                    FirebaseAuth.getInstance().signOut();
+                    startSignUpActivity();
+                    break;
+            }
+        }
+    };
+
+    private void startSignUpActivity(){
+        Intent intent = new Intent(this,SignUpActivity.class);
+        startActivity(intent);
     }
 }
