fc184033f311c650c9f25aa028503195b4b3739f
Daekyue <howeorb@naver.com>
2022-08-19 21:33:27 +0900

    글작성에 이미지 추가

--- app/src/main/java/com/akj/sns_project/WritePostActivity.java
+++ app/src/main/java/com/akj/sns_project/WritePostActivity.java
@@ -1,17 +1,23 @@
 package com.akj.sns_project;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -33,6 +39,33 @@ public class WritePostActivity extends BasicActivity {
         findViewById(R.id.video).setOnClickListener(onClickListener);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 0: {
+                if (resultCode == Activity.RESULT_OK) {
+                    String profilePath = data.getStringExtra("profilePath");
+
+                    LinearLayout parent = findViewById(R.id.contentsLayout);
+
+                    ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                    ImageView imageView = new ImageView(WritePostActivity.this);
+                    imageView.setLayoutParams(layoutParams);
+                    Glide.with(this).load(profilePath).override(1000).into(imageView);
+                    parent.addView(imageView);
+
+                    EditText editText = new EditText(WritePostActivity.this);
+                    editText.setLayoutParams(layoutParams);
+                    editText.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE | InputType.TYPE_CLASS_TEXT);
+                    parent.addView(editText);
+                }
+                break;
+            }
+        }
+    }
+
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -51,14 +84,12 @@ public class WritePostActivity extends BasicActivity {
         }
     };
 
-    private void postUpdate(){ // 글작성
+    private void postUpdate(){ // 글작성     part11 10분 16초
         String title = ((EditText)findViewById(R.id.titleEditText)).getText().toString();
         String contents = ((EditText)findViewById(R.id.contentsEditText)).getText().toString();
 
         if(title.length() > 0 && contents.length() > 0){
             user = FirebaseAuth.getInstance().getCurrentUser();
-
-
             WriteInfo writeInfo  = new WriteInfo(title, contents, user.getUid());
             uploader(writeInfo);
         }else{
@@ -85,7 +116,7 @@ public class WritePostActivity extends BasicActivity {
     private void myStartActivity(Class c, String media ){
         Intent intent = new Intent(this,c);
         intent.putExtra("media",media);
-        startActivity(intent);
+        startActivityForResult(intent, 0);
     }
 
     private void startToast(String msg){
