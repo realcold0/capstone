f93fd48fca37597e36278d12e255f513b0e8a296
Daekyue <howeorb@naver.com>
2022-08-24 21:50:52 +0900

    프로필 사진 기능 추가

---
+++ app/src/main/java/com/akj/sns_project/BasicActivity.java
@@ -0,0 +1,14 @@
+package com.akj.sns_project;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BasicActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
--- app/src/main/java/com/akj/sns_project/MemberInfo.java
+++ app/src/main/java/com/akj/sns_project/MemberInfo.java
@@ -7,18 +7,19 @@ public class MemberInfo {
     private String phoneNubmer;
     private String birthDay;
     private String address;
+    private String photoUrl;
 
-    public MemberInfo(String name, String phoneNubmer, String birthDay, String address){
+    public MemberInfo(String name, String phoneNubmer, String birthDay, String address, String photoUrl){
         this.name = name;
         this.phoneNubmer = phoneNubmer;
         this.birthDay = birthDay;
         this.address = address;
+        this.photoUrl = photoUrl;
     }
 
     public String getName(){
         return this.name;
     }
-
     public void setName(String name){
         this.name = name;
     }
@@ -26,7 +27,6 @@ public class MemberInfo {
     public String getPhoneNubmer(){
         return this.phoneNubmer;
     }
-
     public void setPhoneNubmer(String phoneNubmer){
         this.phoneNubmer = phoneNubmer;
     }
@@ -34,16 +34,13 @@ public class MemberInfo {
     public String getBirthDay(){
         return this.birthDay;
     }
-
     public void setBirthDay(String birthDay){
         this.birthDay = birthDay;
     }
 
-    public String getAddress(){
-        return this.address;
-    }
+    public String getAddress(){return this.address; }
+    public void setAddress(String address){this.address = address;}
 
-    public void setAddress(String address){
-        this.address = address;
-    }
+    public String getPhotoUrl(){return this.photoUrl; }
+    public void setPhotoUrl(String photoUrl){this.photoUrl = photoUrl;}
 }
