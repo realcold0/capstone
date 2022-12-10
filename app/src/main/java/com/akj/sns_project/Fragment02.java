ee4b58a35d07032a02b185879cfa1c653d960545
realcold0 <realcold0@gmail.com>
2022-12-10 21:27:52 +0900

    카테고리 페이지 카테고리 검색 추가

--- app/src/main/java/com/akj/sns_project/Fragment02.java
+++ app/src/main/java/com/akj/sns_project/Fragment02.java
@@ -37,6 +37,7 @@ public class Fragment02 extends Fragment {
     MainActivity mainActivity;
     String language = "&language=ko-KR";
     String url = "https://api.themoviedb.org/3/discover/movie?api_key=3c314dc629a0e72e9328fe7c33981cf2";
+
     // 메인 액티비티 위에 올린다.
     @Override
     public void onAttach(Context context) {
@@ -109,7 +110,6 @@ public class Fragment02 extends Fragment {
         Fragment01 fragment01;
 
 
-
         if (requestQueue == null) {
 
             requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
@@ -125,47 +125,73 @@ public class Fragment02 extends Fragment {
 
                 case R.id.btnAction:
                     genre = "&with_genres=" + 28;
-                    mainActivity.GenreSearch(url+genre+language);
+                    mainActivity.GenreSearch(url + genre + language);
                     break;
                 case R.id.btnAdventure:
                     genre = "&with_genres=" + 12;
-                    mainActivity.GenreSearch(url+genre+language);
+                    mainActivity.GenreSearch(url + genre + language);
                     break;
                 case R.id.btnAni:
                     genre = "&with_genres=" + 16;
-                    mainActivity.GenreSearch(url+genre+language);
+                    mainActivity.GenreSearch(url + genre + language);
                     break;
                 case R.id.btnComedy:
                     genre = "&with_genres=" + 35;
-                    mainActivity.GenreSearch(url+genre+language);
+                    mainActivity.GenreSearch(url + genre + language);
                     break;
                 case R.id.btnCrime:
                     genre = "&with_genres=" + 80;
-                    mainActivity.GenreSearch(url+genre+language);
+                    mainActivity.GenreSearch(url + genre + language);
                     break;
                 case R.id.btnRomance:
                     genre = "&with_genres=" + 10749;
-                    mainActivity.GenreSearch(url+genre+language);
+                    mainActivity.GenreSearch(url + genre + language);
                     break;
                 case R.id.btnSF:
                     genre = "&with_genres=" + 878;
-                    mainActivity.GenreSearch(url+genre+language);
+                    mainActivity.GenreSearch(url + genre + language);
                     break;
                 case R.id.btnMusic:
                     genre = "&with_genres=" + 10402;
-                    mainActivity.GenreSearch(url+genre+language);
+                    mainActivity.GenreSearch(url + genre + language);
                     break;
 
                 case R.id.btn_all_genre:
 
                     mainActivity.GenreSearch("https://api.themoviedb.org/3/trending/movie/week?api_key=3c314dc629a0e72e9328fe7c33981cf2&page=1&language=ko-KR");
                     break;
+                case R.id.btnDocumentary:
+                    genre = "&with_genres=" + 99;
+                    mainActivity.GenreSearch(url + genre + language);
+                    break;
 
+                case R.id.btnHistory:
+                    genre = "&with_genres=" + 36;
+                    mainActivity.GenreSearch(url + genre + language);
+                    break;
+
+                case R.id.btnWar:
+                    genre = "&with_genres=" + 10752;
+                    mainActivity.GenreSearch(url + genre + language);
+                    break;
+                case R.id.btnMystery:
+                    genre = "&with_genres=" + 9648;
+                    mainActivity.GenreSearch(url + genre + language);
+                    break;
+
+                case R.id.btnFantasy:
+                    genre = "&with_genres=" + 14;
+                    mainActivity.GenreSearch(url + genre + language);
+                    break;
+
+                case R.id.btnFamily:
+                    genre = "&with_genres=" + 10751;
+                    mainActivity.GenreSearch(url + genre + language);
+                    break;
 
             }
         }
     };
-
 
 
 }
