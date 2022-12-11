94d2c8d1070effbaa94e51bd30ef75762a28642b
realcold0 <realcold0@gmail.com>
2022-12-11 15:23:07 +0900

    영화 페이지 장르 정보 받아와서 보여주기

---
+++ app/src/main/java/com/akj/sns_project/MovieGenre.java
@@ -0,0 +1,14 @@
+package com.akj.sns_project;
+
+public class MovieGenre {
+    int id;
+    String  name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
